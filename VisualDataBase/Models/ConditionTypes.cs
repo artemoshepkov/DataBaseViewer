7105254bb5da9a42a62db91741db4a955225ccb1
Artem <artemoshepkov790@gmail.com>
2022-06-16 11:39:50 +0300

    Create files for enums

---
+++ VisualDataBase/Models/ConditionTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualDataBase.Models
+{
+    public enum ConditionTypes
+    {
+        SelectType = 0,
+        JoinType,
+        GroupByType
+    }
+}
---
+++ VisualDataBase/Models/TableTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualDataBase.Models
+{
+    public enum TableTypes
+    {
+        Seasons = 0,
+        Nations,
+        Players,
+        PlayersSeasons
+    }
+}
